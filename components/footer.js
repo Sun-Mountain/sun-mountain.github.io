6388e9c3c5beecb2ad5553924d78ce80abaf28c2
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2021-12-20 15:59:11 -0500

    Finish footer

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,26 @@
+import Link from 'next/link';
+import icons from '../public/socialmedia.json';
+import styles from '../styles/footer.module.scss';
 
 export default function Footer() {
+
   return (
     <footer>
-      Footer Component
+      <div className={styles.footer_border}></div>
+      <div className={styles.icons_container}>
+        {icons && icons.length>0 && icons.map(sm => (
+          <div className={styles.single_icon}>
+            <Link href={sm.url}>
+              <a target="_blank">
+                <img
+                  src={sm.logo}
+                  alt={sm.alt}
+                />
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
     </footer>
   )
 }
