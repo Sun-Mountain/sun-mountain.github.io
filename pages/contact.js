885ed6bfc2b7ea46c3c19ce426647cfc793088ba
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-01-24 17:31:28 -0500

    Update contact page

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout from '../components/layout'
 // import Link from 'next/link'
 
+import style from '../styles/contact.module.scss'
+
 export default function Contace() {
   return (
     <Layout contact>
@@ -9,7 +11,12 @@ export default function Contace() {
         <title>Contact</title>
       </Head>
       <section>
-        Contact Coming soon.
+        <div className={style.contact_card}>
+          <h2>Recruiter?</h2>
+          While I am <strong>currently not looking for opportunities</strong>, you can direct all inquiries to <a href="https://forms.gle/Ef6rMi2uoUpRk7AW6" target="_blank">this form</a>.<br /><br />
+          
+          If / when I <strong>am</strong> looking for a new adventure, I will go through this list first. :)
+        </div>
       </section>
     </Layout>
   )
