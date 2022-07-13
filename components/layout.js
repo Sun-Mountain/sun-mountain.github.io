c292f183580fc4d696377098fd39b882bf72f10f
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-07-13 09:37:39 -0400

    hard code content

--- components/layout.js
+++ components/layout.js
@@ -4,13 +4,16 @@ import styles from '../styles/layout.module.scss';
 import Footer from './footer';
 import Nav from './nav';
 
-export const siteTitle = 'Nicole Zonnenberg Portfolio';
-
 export default function Layout({ children }) {
   return (<div className={styles.container}>
       <Head>
         <html lang="eng-US" />
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Personal portfolio for Nicole Zonnenberg."
+        />
+        <meta name="og:title" content='Nicole Zonnenberg Portfolio' />
       </Head>
       <Nav />
       <main>
