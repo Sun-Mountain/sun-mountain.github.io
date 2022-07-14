e92a66553fad42b9033d21cfda3c3fd01015b077
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-07-14 16:06:04 -0400

    Remove about link/route

---
+++ src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<!-- <script setup>
+  import {RouterLink} from 'vue-router'
+</script> -->
+
+<template>
+  <nav>
+    <div>
+      <img alt="Vue logo" src="../assets/images/rainbow-sheep.gif">
+    </div>
+  </nav>
+</template>
+
+<style>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/images/rainbow-sheep.gif">
+    <div class="hello">
+      <h1>Nicole Zonnenberg</h1>
+      <h2><a href="https://fearless.tech/">Fearless</a> Software Engineer</h2>
+      <div>
+        <h3>Selected Writings</h3>
+        <p>
+          <a href="https://dev.to/nikacodes/basic-color-theory-for-web-developers-15a0">Basic Color Theory for Web Developers</a>
+        </p>
+        <p>
+          <a href="https://technical.ly/software-development/nicole-zonnenberg-software-career/">How I Got Here: From a chocolate factory to a software firm</a>
+        </p>
+        <p>
+          <a href="https://fearless.tech/blog/2020/09/07/what-is-tech-debt">What is Tech Debt?</a>
+        </p>
+      </div>
+      <div>
+        <h3>Awards</h3>
+        <p>
+          <a href="https://technical.ly/software-development/reallist-engineers-20200-influential-technologists/">Techincal.ly RealLIST Engineer (2020)</a>
+        </p>
+        <p>
+          <a href="https://fearless.tech/blog/2021/02/22/celebrating-the-fearless-value-award-winners">Fearless Open Dialogue Award (2021)</a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
