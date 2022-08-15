d4ba835cf934070bc4601f7ea8d98d4c6d3a5a69
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-08-15 17:44:43 -0400

    fix main

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <template>
   <main>
-    This is my main
+    <h3>Selected Writings</h3>
+    <a href="https://dev.to/nikacodes/basic-color-theory-for-web-developers-15a0">Basic Color Theory for Web Developers</a>
+    <a href="https://technical.ly/software-development/nicole-zonnenberg-software-career/">How I Got Here: From a chocolate factory to a software firm</a>
+    <a href="https://fearless.tech/blog/2020/09/07/what-is-tech-debt">What is Tech Debt?</a>
+    <h3>Awards</h3>
+    <a href="https://technical.ly/software-development/reallist-engineers-20200-influential-technologists/">Techincal.ly RealLIST Engineer (2020)</a>
+    <a href="https://fearless.tech/blog/2021/02/22/celebrating-the-fearless-value-award-winners">Fearless Open Dialogue Award (2021)</a>
   </main>
 </template>
 
