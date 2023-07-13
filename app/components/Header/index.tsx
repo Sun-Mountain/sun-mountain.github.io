4d3b76fccaa890fae0df4f9a350d4a1238442320
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2023-07-13 13:10:28 -0400

    add section title

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -3,9 +3,7 @@ import Link from "next/link"
 export default function Header () {
   return (
     <section>
-      <div className='header-title'>
-        <strong>welcome</strong>.
-      </div>
+      <h1 className="header-title">welcome.</h1>
       <div>
         I&apos;m Nicole Zonnenberg, a software engineer <Link href='https://fearless.tech/' target='_blank'>@Fearless</Link>.
       </div>
--- app/components/Projects/index.tsx
+++ app/components/Projects/index.tsx
@@ -4,6 +4,7 @@ import ProjectCard from '../ProjectCard'
 export default function Projects () {
   return (
     <section>
+      <h2 className='section-title'>Selected Projects</h2>
       <div className='cards-wrapper'>
         {ProjectList.map((project, index) => {
           return <ProjectCard key={index} project={project} />
