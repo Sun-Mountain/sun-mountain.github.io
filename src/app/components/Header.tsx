c1f7e73ded8eef5c3e02a85ee56f591aae357951
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-28 14:18:41 -0400

    add transition

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,19 @@
 
 const Header = () => {
 
-  const title = "PORTFOLIO".split('');
+  const title = "PORTFOLIO.".split('');
 
   return (
     <div id="header">
       <h1>
-        {title.map((letter, index) => {
-          return <span key={index}>{letter}</span>
-        })}
+        <div className="surtitle">
+          welcome to my
+        </div>
+        <div id="title" className="expand-on-hover">
+          {title.map((letter, index) => {
+            return <span key={index}>{letter}</span>
+          })}
+        </div>
       </h1>
     </div>
   )
