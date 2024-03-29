36d5707d02db56e71292415988db05fd336aafa3
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-03-29 16:32:58 -0400

    add intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,11 @@ import FindMe from "@/components/FindMe";
 export default function Home() {
   return (
     <>
+      <section>
+        Hello, World! I am <strong>Nicole Zonnenberg</strong>.<br /><br />
+        I am a <strong>Full Stack Developer</strong> with a passion for visibility, accessibility, and equity in tech.<br /><br />
+        Below you can find some select projects I have worked on, articles I have written, and where to find me online.
+      </section>
       <Portfolio />
       <Articles />
       <FindMe />
